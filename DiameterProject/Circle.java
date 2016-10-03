02ec66044eedd17c2a20c8735a0fabc47d527f93
DrBenCarson <adrianoz@comcast.net>
2016-10-03 12:28:26 -0600

    Added

---
+++ DiameterProject/Circle.java
@@ -0,0 +1,14 @@
+
+public class Circle {
+	private double radius;
+
+public Circle(double r)
+{
+	radius=r;
+}
+public double diameter(double diameter)
+{
+	diameter=(2*radius);
+	return diameter;
+}
+}
---
+++ DiameterProject/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Circle myCircle=new Circle(7.1);
+		double d=myCircle.diameter(7.1);
+		System.out.println(d);
+	}
+
+}
